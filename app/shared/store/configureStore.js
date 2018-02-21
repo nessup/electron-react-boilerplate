6cb325a1f884554b31a2fc818a3a2be0cee5cfa7
Danny Friday <nessup@gmail.com>
2018-02-21 15:00:27 -0700

    Combine prod and dev `configureStore` into one file
    This reduces likelihood of divergence between the two

--- app/shared/store/configureStore.js
+++ app/shared/store/configureStore.js
@@ -1,6 +1,68 @@
-// @flow
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./configureStore.prod'); // eslint-disable-line global-require
-} else {
-  module.exports = require('./configureStore.dev'); // eslint-disable-line global-require
-}
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import { createHashHistory } from 'history';
+import { routerMiddleware, routerActions } from 'react-router-redux';
+import { createLogger } from 'redux-logger';
+import rootReducer from '../reducers';
+import * as counterActions from '../actions/counter';
+import type { counterStateType } from '../reducers/counter';
+
+const history = createHashHistory();
+
+const configureStore = (initialState?: counterStateType) => {
+  // Redux Configuration
+  const middleware = [];
+  const enhancers = [];
+
+  // Thunk Middleware
+  middleware.push(thunk);
+
+  if (process.env.NODE_ENV !== 'production') {
+    // Logging Middleware
+    const logger = createLogger({
+      level: 'info',
+      collapsed: true
+    });
+
+    // Skip redux logs in console during the tests
+    if (process.env.NODE_ENV !== 'test') {
+      middleware.push(logger);
+    }
+  }
+
+  // Router Middleware
+  const router = routerMiddleware(history);
+  middleware.push(router);
+
+  // Redux DevTools Configuration
+  const actionCreators = {
+    ...counterActions,
+    ...routerActions,
+  };
+  // If Redux DevTools Extension is installed use it, otherwise use Redux compose
+  /* eslint-disable no-underscore-dangle */
+  let composeEnhancers = compose;
+  if (process.env.NODE_ENV !== 'production' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) {
+    composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+      // Options: http://zalmoxisus.github.io/redux-devtools-extension/API/Arguments.html
+      actionCreators,
+    });
+  }
+  /* eslint-enable no-underscore-dangle */
+
+  // Apply Middleware & Compose Enhancers
+  enhancers.push(applyMiddleware(...middleware));
+  const enhancer = composeEnhancers(...enhancers);
+
+  // Create Store
+  const store = createStore(rootReducer, initialState, enhancer);
+
+  if (process.env.NODE_ENV === 'development' && module.hot) {
+    module.hot.accept('../reducers', () =>
+      store.replaceReducer(require('../reducers'))); // eslint-disable-line global-require
+  }
+
+  return store;
+};
+
+export default { configureStore, history };
