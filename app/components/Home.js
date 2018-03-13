e0c82c7aade3da3ba95a1af770a33ec00842d4c4
Danny Friday <nessup@gmail.com>
2018-03-13 14:20:41 -0700

    Include semantic-ui-less, build with Webpack

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,6 +1,7 @@
 // @flow
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { Button } from 'semantic-ui-react';
 import styles from './Home.css';
 
 type Props = {};
@@ -14,6 +15,7 @@ export default class Home extends Component<Props> {
         <div className={styles.container} data-tid="container">
           <h2>Home</h2>
           <Link to="/counter">to Counter</Link>
+          <Button primary>Primary test button</Button>
         </div>
       </div>
     );
